f7dc989cc53a2e40abfcd214fe328079adafd6cf
TheShadowsSpeak <69861459+TheShadowsSpeak@users.noreply.github.com>
2021-02-04 20:26:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,24 +30,9 @@ function draw() {
   background(46, 139, 87)
   
   drawSprites();
-  function ReadStock(data){
-    FoodS=data.val();
-  }
-  function WriteStock(x){
-    if(x<=0){
-      x=0
-    }
-    else{
-      x=x-1
-    }
-    database.ref('/').update({
-      Food:x
-    })
-    
-  }
-  ReadStock()
-  WriteStock()
-  if(keyWentDown(UP_ARROW)&&frameCount%400===0){
+  
+  if(keyWentDown(UP_ARROW)){
+    console.log(FoodS)
     dog.addImage(HappyDog)
     WriteStock(FoodS)
   }
@@ -56,8 +41,24 @@ function draw() {
   textFont("Baskerville")
   fill("Brown")
   text("NOTE- Every day(400 Frames), your Dog must be fed. Press the UP Arrow Key to feed",10,100)
-  text("Food:"+FoodS, 250,200)
+  text("Food:"+FoodS, 250,130)
 }
 
+function ReadStock(data){
+  if(data)
+  FoodS=data.val();
+}
+  
 
-
+function WriteStock(x){
+  if(x<=0){
+    x=0
+  }
+  else{
+    x=x-1
+  }
+  Database.ref('/').update({
+    Food:x
+  })
+  
+}
